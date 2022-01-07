f1ba4ab9ef9566aeaaa624aa97f60c458e880dc5
diaznet <jd@diaznet.fr>
2022-01-07 11:18:25 +0100

    Added support for TFA PUSH and EMAIL types.

--- arlo-downloader.py
+++ arlo-downloader.py
@@ -1,24 +1,54 @@
 #!/usr/bin/env python
-#
 
 import logging
 import os
 import sys
-import time
-import pprint
 import pyaarlo
 import asyncio 
 import argparse
 
+from config import Config
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Monitor Arlo Cameras.')
+    
+    parser = argparse.ArgumentParser(description='Download records from Arlo Cameras.')
     parser.add_argument('-d',
                        '--debug',
                        action='store_true',
                        help='Enable Debug messages')
     parser.add_argument('-m',
-                        '--save-media',
-                        help='Save Media naming scheme',
+                        '--save-media-to',
+                        help="Save Media naming scheme (default = \'{}\')".format(Config.config("save_media_to")),
+                        action='store')
+    parser.add_argument('-t',
+                        '--tfa-type',
+                        choices=['PUSH', 'EMAIL'],
+                        help="Set TFA type (default = \'{}\')".format(Config.config("tfa_type")),
+                        action='store')
+    parser.add_argument('-s',
+                        '--tfa-source',
+                        choices=['push', 'imap'],
+                        help="Set TFA source (default = \'{}\')".format(Config.config("tfa_source")),
+                        action='store')
+    parser.add_argument('-r',
+                        '--tfa-retries',
+                        help="Set TFA max retries (default = {}).".format(Config.config("tfa_retries")),
+                        action='store')
+    parser.add_argument('-T',
+                        '--tfa-timeout',
+                        help="Set TFA timeout (default = {}).".format(Config.config("tfa_timeout")),
+                        action='store')
+    parser.add_argument('-H',
+                        '--tfa-host',
+                        help="(EMAIL/imap only) Set TFA host (default = {}).".format(Config.config("tfa_host")),
+                        action='store')
+    parser.add_argument('-U',
+                        '--tfa-username',
+                        help="(EMAIL/imap only) Set TFA username (default = {}).".format(Config.config("tfa_username")),
+                        action='store')
+    parser.add_argument('-P',
+                        '--tfa-password',
+                        help="(EMAIL/imap only) Set TFA password (default = {}).".format(Config.config("tfa_password")),
                         action='store')
     args = parser.parse_args()
     return args
@@ -33,7 +63,7 @@ def set_logger(dbg):
                         )
     _LOGGER = logging.getLogger(__file__) 
 
-def Init(media_location):
+def Init():
 
     # code to trap when attributes change
     def attribute_changed(device, attr, value):
@@ -46,8 +76,19 @@ def Init(media_location):
     # add `verbose_debug=True` to enable even more debugging
     # add `dump=True` to enable event stream packet dumps
     arlo = pyaarlo.PyArlo(username=USERNAME, password=PASSWORD,
-                        tfa_type='SMS', tfa_source='console', synchronous_mode=True,
-                        save_state=False, dump=False, storage_dir='aarlo', verbose_debug=True, save_media_to=media_location)
+                        tfa_type=Config.config('tfa_type'),
+                        tfa_source=Config.config('tfa_source'),
+                        tfa_retries=Config.config('tfa_retries'),
+                        tfa_timeout=Config.config('tfa_timeout'),
+                        tfa_host=Config.config('tfa_host'),
+                        tfa_username=Config.config('tfa_username'),
+                        tfa_password=Config.config('tfa_password'),
+                        synchronous_mode=True,
+                        save_state=False,
+                        dump=False,
+                        storage_dir='aarlo',
+                        verbose_debug=True,
+                        save_media_to=Config.config('save_media_to'))
     if not arlo.is_connected:
         logging.info("failed to login({})".format(arlo._last_error))
         sys.exit(-1)
@@ -64,26 +105,38 @@ def Init(media_location):
         camera.add_attr_callback('*', attribute_changed)
 
 def main(arguments):
-    script_path = os.path.dirname(os.path.realpath(__file__))
-    filename_pattern = '${Y}/${m}/${F}T${t}_${N}_${SN}.mp4'
-    
-    save_media_to = script_path + filename_pattern
-    
-    #set_logger(False)
-    
+     
     loop = asyncio.get_event_loop()
-    if arguments.save_media:
-        save_media_to = arguments.save_media
+    if arguments.save_media_to:
+        Config.set("save_media_to", arguments.save_media_to)
+    if arguments.tfa_type:
+        Config.set("tfa_type", arguments.tfa_type)
+    if arguments.tfa_source:
+        Config.set("tfa_source", arguments.tfa_source)
+    if arguments.tfa_retries:
+        Config.set("tfa_retries", arguments.tfa_retries)
+    if arguments.tfa_timeout:
+        Config.set("tfa_timeout", arguments.tfa_timeout)
+    if arguments.tfa_host:
+        Config.set("tfa_host", arguments.tfa_host)
+    if arguments.tfa_username:
+        Config.set("tfa_username", arguments.tfa_username)
+    if arguments.tfa_password:
+        Config.set("tfa_password", arguments.tfa_password)
     if arguments.debug:
         set_logger(True)
     else:
         set_logger(False)
     try:
-        Init(save_media_to)
+        Init()
         loop.run_forever()
+    except KeyboardInterrupt:
+        sys.exit(0)
     finally:
         loop.close()
 
 if __name__ == "__main__":
+
     arguments = parse_arguments()
     main(arguments)
+ 
---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+  __conf = {
+    "save_media_to": os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "${Y}/${m}/${F}T${t}_${N}_${SN}.mp4",
+    "tfa_type": "PUSH",
+    "tfa_source": "push",
+    "tfa_retries": 10,
+    "tfa_timeout": 5,
+    "tfa_host": '',
+    "tfa_username": '',
+    "tfa_password": ''
+  }
+  __setters = [
+    "set_logger",
+    "save_media_to",
+    "tfa_type",
+    "tfa_source",
+    "tfa_retries",
+    "tfa_timeout",
+    "tfa_host",
+    "tfa_username",
+    "tfa_password"
+    ]
+  @staticmethod
+  def config(name):
+    return Config.__conf[name]
+
+  @staticmethod
+  def set(name, value):
+    if name in Config.__setters:
+      Config.__conf[name] = value
+    else:
+      raise NameError("Name not accepted in set() method")
